f6beaae853f9c97c2b3d081be8f8d6e89f25f047
shhiivm <shhiivm@pm.me>
2025-09-18 18:43:46 +0530

    js to jsx file

---
+++ src/Data.jsx
@@ -0,0 +1,83 @@
+import "./projects/PasswordGenerator";
+import PasswordGenerator from "./projects/PasswordGenerator";
+import { Route } from "react-router-dom";
+
+export const projectList = [
+  {
+    title: "Color Flipper",
+    download_url: "../jsProjects/01 Color Flipper/Result.png",
+    description: "Click on the coloured box to change background color.",
+    link: "https://github.com/shhiivm/myProjects/tree/main/01%20Color%20Flipper",
+    live: "../jsProjects/01 Color Flipper/index.html",
+  },
+  {
+    title: "BMI calculator",
+    download_url:
+      "../jsProjects/02 BMI calculator/Screenshot 2025-09-15 131307.png",
+    description: "A simple BMI Calculator app built using JavaScript.",
+    link: "https://github.com/shhiivm/myProjects/tree/main/02%20BMI%20calculator",
+    live: "../jsProjects/02 BMI calculator/index.html",
+  },
+  {
+    title: "Digital Clock",
+    download_url:
+      "../jsProjects/03 Digital Clock/Screenshot 2025-09-15 131346.png",
+    description:
+      "A JavaScript digital clock is a simple web-based clock implemented using plain JavaScript",
+    link: "https://github.com/shhiivm/myProjects/tree/main/03%20Digital%20Clock",
+    live: "../jsProjects/03 Digital Clock/index.html",
+  },
+  {
+    title: "To-Do List App",
+    download_url: "../jsProjects/07 Todo List/Screenshot 2025-09-16 163158.png",
+    description:
+      "A simple task management app built using JavaScript.It helps users manage their daily tasks efficiently. It allows adding and deleting tasks, providing a simple and organized way to boost productivity",
+    link: "https://github.com/shhiivm/myPortfolio/tree/main/jsProjects/07%20Todo%20List",
+    live: "../jsProjects/07 Todo List/index.html",
+  },
+  {
+    title: "Guess a Number",
+    download_url:
+      "../jsProjects/05 Guess a Number/Screenshot 2025-09-15 131635.png",
+    description:
+      "A fun number-guessing game where the player tries to guess a number between 1 to 100, receiving hints like 'too high' or 'too low' until the correct answer is found.",
+    link: "https://github.com/shhiivm/myPortfolio/tree/main/jsProjects/05%20Guess%20a%20Number",
+    live: "../jsProjects/05 Guess a Number/index.html",
+  },
+  {
+    title: "Random Quote Generator",
+    download_url:
+      "../jsProjects/06 Random Quote/Screenshot 2025-09-15 133123.png",
+    description:
+      "A random quote generator that fetches quotes from an external API and displays them dynamically. Each click provides a new inspirational or thought-provoking quote",
+    link: "https://github.com/shhiivm/myPortfolio/tree/main/jsProjects/06%20Random%20Quote",
+    live: "../jsProjects/06 Random Quote/index.html",
+  },
+  {
+    title: "Random Password Generator",
+    download_url:
+      "../jsProjects/09 Random Password Generator/Screenshot 2025-09-16 163703.png",
+    description:
+      "A sleek, web-based password generator that allows users to create strong, random passwords. Features with a copy-to-clipboard option and real-time password display for easy use",
+    link: "https://github.com/shhiivm/myPortfolio/tree/main/jsProjects/09%20Random%20Password%20Generator",
+    live: `../jsProjects/09 Random Password Generator/index.html`,
+  },
+  {
+    title: "QR Code Generator",
+    download_url:
+      "../jsProjects/04 QR Code Generator/Screenshot 2025-09-15 131525.png",
+    description:
+      "A QR Code Generator using an API is a web or application feature that dynamically creates QR codes by sending data to an external QR code generation service through an API.",
+    link: "https://github.com/yourgithub/qr-generator",
+    live: "../jsProjects/04 QR Code Generator/index.html",
+  },
+  {
+    title: "Weather App",
+    download_url:
+      "../jsProjects/11 Weather App/Screenshot 2025-09-16 162906.png",
+    description:
+      "A weather app powered by a weather API that fetches real-time weather data for any city or location. It displays temperature, conditions, and other details in a simple, responsive interface.",
+    link: "https://github.com/shhiivm/myPortfolio/tree/main/jsProjects/11%20Weather%20App",
+    live: "../jsProjects/11 Weather App/index.html",
+  },
+];
